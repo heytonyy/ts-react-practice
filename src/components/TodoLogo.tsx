b90f42ff239918dc9238f5759a6f1fbaf486cf55
Tony Aiello <heytonyyy3@gmail.com>
2022-12-25 10:54:40 -0800

    learning typescript & practice react with mui

---
+++ src/components/TodoLogo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const TodoLogo = () => {
+    return (
+        <Box>
+            <h1>Todo Logo</h1>
+        </Box>
+    )
+}
+
+export default TodoLogo
---
+++ src/views/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InputTodo from '../components/InputTodo'
+import TodoLogo from '../components/TodoLogo'
+
+const Main = () => {
+  return (
+    <>
+        <TodoLogo />
+        <InputTodo />
+    </>
+  )
+}
+
+export default Main
